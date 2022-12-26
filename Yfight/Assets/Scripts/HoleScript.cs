2ab96e637e7b24b4ea238f002edc88cb5b68df31
JoWi2404 <jwiryawan@student.ciputra.ac.id>
2022-12-26 19:33:43 +0700

    Stage 2 Update

---
+++ Yfight/Assets/Scripts/HoleScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleScript : MonoBehaviour
+{
+    //Players when fall to hole 
+  private void OnCollisionEnter2D(Collision2D other) {
+    
+  }
+}
